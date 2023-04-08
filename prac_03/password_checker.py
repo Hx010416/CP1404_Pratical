a9a7f6279b58c16d6fa042a0d6076c99e7ea4a83
Hx010416 <xiao.hu@my.jcu.edu.au>
2023-04-08 23:49:35 +0800

    Check if password is possible.

---
+++ prac_03/password_checker.py
@@ -0,0 +1,67 @@
+MIN_LENGTH = 2
+MAX_LENGTH = 6
+SPECIAL_CHARS_REQUIRED = False
+SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
+
+
+def main():
+    """Program to get and check a user's password."""
+    print("Please enter a valid password")
+    print("Your password must be between", MIN_LENGTH, "and", MAX_LENGTH,
+          "characters, and contain:")
+    print("\t1 or more uppercase characters")
+    print("\t1 or more lowercase characters")
+    print("\t1 or more numbers")
+    if SPECIAL_CHARS_REQUIRED:
+        print("\tand 1 or more special characters: ", SPECIAL_CHARACTERS)
+    password = input("> ")
+    while not is_valid_password(password):
+        print("Invalid password!")
+        password = input("> ")
+    print(f"Your {len(password)}-character password is valid: {password}")
+
+    # Write user's name to a file
+    name = input("Enter your name: ")
+    with open("name.txt", "w") as name_file:
+        name_file.write(name)
+
+    # Read user's name from file and print it
+    with open("name.txt", "r") as name_file:
+        user_name = name_file.readline().strip()
+    print(f"Your name is {user_name}")
+
+
+def is_valid_password(password):
+    """Determine if the provided password is valid."""
+    # check length
+    if len(password) < MIN_LENGTH or len(password) > MAX_LENGTH:
+        return False
+
+    count_lower = 0
+    count_upper = 0
+    count_digit = 0
+    count_special = 0
+    for char in password:
+        # count each kind of character
+        if char.islower():
+            count_lower += 1
+        elif char.isupper():
+            count_upper += 1
+        elif char.isdigit():
+            count_digit += 1
+        elif char in SPECIAL_CHARACTERS:
+            count_special += 1
+
+    # check counts
+    if count_lower == 0 or count_upper == 0 or count_digit == 0:
+        return False
+
+    # check special characters
+    if SPECIAL_CHARS_REQUIRED and count_special == 0:
+        return False
+
+    # if we get here (without returning False), then the password must be valid
+    return True
+
+
+main()
