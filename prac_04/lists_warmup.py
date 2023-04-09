7f797a13b459ba8b6538dd2cf6df3037fa2f1c94
Hx010416 <xiao.hu@my.jcu.edu.au>
2023-04-09 23:06:41 +0800

    Warm-Up

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,16 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# Test expressions
+print(numbers[0])      # Output: 3
+print(numbers[-1])     # Output: 2
+print(numbers[3])      # Output: 1
+print(numbers[:-1])    # Output: [3, 1, 4, 1, 5, 9]
+print(numbers[3:4])    # Output: [1]
+print(5 in numbers)    # Output: True
+print(7 in numbers)    # Output: False
+print("3" in numbers)  # Output: False
+print(numbers + [6, 5, 3])  # Output: [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+numbers[0] = "ten"     # Change the first element of numbers to "ten" (the string, not the number 10)
+numbers[-1] = 1        # Change the last element of numbers to 1
+print(numbers[2:])     # Print all the elements from numbers except the first two (slice)
+print(9 in numbers)    # Print whether 9 is an element of numbers
