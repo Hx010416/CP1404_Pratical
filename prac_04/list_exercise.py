57b190732697feca69897d4405cafc28afe1fb61
Hx010416 <xiao.hu@my.jcu.edu.au>
2023-04-09 23:09:37 +0800

    Basic list operations and inadequate security checker

--- prac_04/list_exercise.py
+++ prac_04/list_exercise.py
@@ -2,7 +2,7 @@ def main():
 
     numbers = []
     for i in range(5):
-        number = int(input("Number: "))
+        number = int(input("Number:"))
         numbers.append(number)
 
     print(f"The first number is {numbers[0]}")
